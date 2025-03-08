1d4364e270f511e998dcfc3ae0865b247e364d80
Brisa <ccbc@hawaii.edu>
2025-03-08 00:58:39 +0000

    Updated, Cloudiness , removed Gusts, and replaced secret for API Key

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# weather-by-city
-Code displays a simple HTML page to obtain a city weather using Open Weather Map API.
+# weather-by-latitude-longitude
+Code displays a simple HTML page to obtain weather using latitude and longitude, with the Open Weather Map API.
 Note: Requires API Token Key by registration with site below. Free for basic usage
 https://openweathermap.org/api
--- index.js
+++ index.js
@@ -2,41 +2,40 @@
 Name: Brisa Carter
 Assignment: Week 8 - Weather by Latitude and Longitude
 Description: Find the weather by latitude and longitude using the OpenWeather API
-Date: 03/01/25
+Date: 03/07/25
 */
 
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const mySecret = "126806867269e4a740f6fdd87c15f4fb"; //REPLACE with a Seecret!!!
-
+const mySecret = process.env["week8"];
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Displays index.html of root path
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 // Invoked after hitting go in the html form
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
   // Takes in the longitude and latitude from the html form
   const longInput = String(req.body.longInput);
   const latInput = String(req.body.latInput);
   console.log(req.body.longInput);
   console.log(req.body.latInput);
-  
+
   // Build up the URL for the JSON query
   const units = "imperial";
   const apiKey = mySecret;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latInput}&lon=${longInput}&units=${units}&appid=${apiKey}`;
 
   // This gets the data from OpenWeather API
-  https.get(url, function(response) {
+  https.get(url, function (response) {
     console.log(response.statusCode);
 
-    response.on("data", function(data) {
+    response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       if (weatherData.cod === 200) {
         const temp = weatherData.main.temp;
@@ -44,18 +43,26 @@ app.post("/", function(req, res) {
         const city = weatherData.name;
         const humidity = weatherData.main.humidity;
         const speed = weatherData.wind.speed;
-        const gust = weatherData.wind.gust;
+        const cloudiness = weatherData.clouds.all;
         const weatherDescription = weatherData.weather[0].description;
         const icon = weatherData.weather[0].icon;
         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
         // Displays the output of the results
         res.write(`<h1>The weather is ${weatherDescription}</h1>`);
-        res.write(`<h2>The Temperature in ${city} is ${temp} &deg;F but...</h2>`);
-        res.write(`<h2>it feels like ${feels_like} &deg;F</h2>`);
-        res.write(`<h2>The Humidity is ${humidity} %</h2>`);
-        res.write(`<h2>The Wind Speed is ${speed} mph, with Wind Gusts of ${gust} mph</h2>`);
         res.write(`<img src=${imageURL}>`);
+        res.write(
+          `<h2>The Temperature in ${city} is ${temp} &deg;F, and it feels like ${feels_like} &deg;F.</h2>`,
+        );
+      
+        res.write(`<h2>The Humidity is ${humidity} %.</h2>`);
+        res.write(
+          `<h2>The Wind Speed is ${speed} mph.</h2>`,
+        );
+        res.write(
+          `<h2>The Cloudiness is  ${cloudiness} %.</h2>`,
+        );
+
         res.send();
       } else {
         res.write("<h1>Location not found!</h1>");
@@ -66,6 +73,6 @@ app.post("/", function(req, res) {
 });
 
 // Code will run on port 3000 or any available open port
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port");
 });
